05b871a45a496344b201966f24bdd1409fd46e70
mzhdipu <mojammelhossaindipu@gmail.com>
2022-11-08 19:38:47 +0600

    updated header menu

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -6,7 +6,7 @@ import { menuItems } from './MenuItems';
 const Header = () => {
 
     return (
-        <header className="pb-6 bg-white lg:pb-0">
+        <header className="pb-6 bg-white lg:pb-0 shadow-md">
             <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                 
                 <nav className="flex items-center justify-between h-16 lg:h-20">
@@ -34,7 +34,7 @@ const Header = () => {
                     </ul>
 
                     
-                    <Link className="items-center justify-center hidden px-4 py-3 ml-10 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md lg:inline-flex hover:bg-blue-700 focus:bg-blue-700" role="button"> Get started now</Link>
+                    <Link className="items-center justify-center hidden px-4 py-3 ml-10 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md lg:inline-flex hover:bg-blue-700 focus:bg-blue-700" role="button"> Login </Link>
                 </nav>
 
                 
@@ -46,7 +46,7 @@ const Header = () => {
                     </div>
 
                     <div className="px-6 mt-6">
-                        <Link className="inline-flex justify-center px-4 py-3 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md tems-center hover:bg-blue-700 focus:bg-blue-700" role="button"> Get started now</Link>
+                        <Link className="inline-flex justify-center px-4 py-3 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md tems-center hover:bg-blue-700 focus:bg-blue-700" role="button"> Login </Link>
                     </div>
                 </nav>
             </div>
--- src/components/Shared/Header/MenuItems.js
+++ src/components/Shared/Header/MenuItems.js
@@ -13,4 +13,18 @@ export const menuItems = <>
     <li className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
         <Link to='/blog'>Blog</Link>
     </li>
+
+    <li className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+        <Link to='/add/services'>Add Services</Link>
+    </li>
+
+    <li className="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600">
+        <Link to='/login'>Login</Link>
+    </li>
+
+    <li tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img src="https://placeimg.com/80/80/people" />
+        </div>
+    </li>
 </>
