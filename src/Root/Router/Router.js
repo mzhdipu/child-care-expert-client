aed0ec407fd363eb60853e51e9d685995841fc55
mzhdipu <mojammelhossaindipu@gmail.com>
2022-11-08 19:53:13 +0600

    service, blog page desing done

--- src/Root/Router/Router.js
+++ src/Root/Router/Router.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Blog from "../../Pages/Blog/Blog";
 import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
+import AddServices from "../../Pages/Services/AddServices/AddServices";
 import Services from "../../Pages/Services/Services";
 import Main from "../Layout/Main";
 
@@ -24,6 +26,14 @@ export const router = createBrowserRouter([
             {
                 path : "/blog",
                 element : <Blog></Blog>
+            },
+            {
+                path : "/add/services",
+                element : <AddServices></AddServices>
+            },
+            {
+                path : "/login",
+                element : <Login></Login>
             },
         ]
     }
