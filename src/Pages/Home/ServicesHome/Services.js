458d247d4f328a85807e8221ce6b9d9b71e59a5a
mzhdipu <mojammelhossaindipu@gmail.com>
2022-11-09 20:25:35 +0600

    service descption slice

--- src/Pages/Home/ServicesHome/Services.js
+++ src/Pages/Home/ServicesHome/Services.js
@@ -28,7 +28,9 @@ const Services = () => {
                                         {service.title} 
                                     </p>
                                     <p className="mt-4 text-base text-gray-600">
-                                        {service.description}
+                                        {
+                                        service.description.length > 100 ? service.description.slice(0, 100) : service.description
+                                        }
                                     </p>
     
                                     <span title="" className="inline-flex items-center justify-center pb-0.5 mt-5 text-base font-semibold text-blue-600 transition-all duration-200 border-b-2 border-transparent hover:border-blue-600 focus:border-blue-600">
